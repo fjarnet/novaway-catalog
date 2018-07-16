9a6e4fe2e4683c63797f90dee12ef6e111b9628d
Fabien Jarnet <fjarnet@clever-age.com>
2018-07-16 19:01:06 +0200

    HP : List last articles + WIP Search form

---
+++ src/AppBundle/Form/SearchFullTextType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace AppBundle\Form;
+
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class SearchFullTextType extends AbstractType
+{
+
+    /**
+     * @param \Symfony\Component\Form\FormBuilderInterface $builder
+     * @param array                                        $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+          ->add('q', TextType::class, [
+            'label' => 'search_form.q.label',
+            'attr' => [
+              'class' => 'form-control',
+            ],
+          ])
+          ->add('submit', SubmitType::class, [
+            'label' => 'search_form.submit.label',
+            'attr' => [
+              'class' => 'btn btn-primary',
+            ],
+          ])
+        ;
+    }
+}
